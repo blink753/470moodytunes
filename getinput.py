fa025eb8e9e407b3d4b17cf1e919924b2db7d16a
Zach Pollock <blink753@tamu.edu>
2012-11-28 09:45:42 -0600

    Simple python for getting imput from the keyboard
    I need to get a GUI going but this is step 1

---
+++ getinput.py
@@ -0,0 +1,4 @@
+mood = raw_input("What is your mood? \r\n")
+describe = raw_input("What other words would describe your mood? \r\n")
+print "Your mood is: ",mood
+print "Other words that describe your mood: ",describe
