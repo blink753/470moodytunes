270b960208137b2072246444f17ee3c26382ecb2
Anh Pham <aeris@neo.tamu.edu>
2012-12-03 03:01:08 -0600

    changed training_set format, fixed unittest, wrote part of the flow

--- utils.py
+++ utils.py
@@ -4,19 +4,8 @@
 import ujson
 import fileinput
 
-def read_tweets():
-    for line in fileinput.input():
+def read_tweets(filename):
+    for line in fileinput.input(filename):
         yield ujson.loads(line)
-        
-# def read_songs(filename):
-    # json_file = open('training_set.json')
-    # data = ujson.load(json_file)
-    # json_file.close()
-    # return data
-    
-def read_songs(filename):
-    json_file = open('training_set.json')
-    for line in iter(json_file):
-        yield ujson.loads(line)
-    json_file.close()
+
     
